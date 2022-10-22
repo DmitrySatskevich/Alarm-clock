bda54fe19f6b7d88e180a0b77bb63ccc1191c536
RecrutSatsckevich <100848161+RecrutSatsckevich@users.noreply.github.com>
2022-10-22 18:40:12 +0300

    Project in progress
    We need to add a check on textField for different input values

--- Alarm-clock/Alarm-clock/ViewController.swift
+++ Alarm-clock/Alarm-clock/ViewController.swift
@@ -11,30 +11,32 @@ class ViewController: UIViewController {
 
     @IBOutlet private var progressViewOutlet: UIProgressView!
     @IBOutlet private var sliderOutlet: UISlider!
-    @IBOutlet private var textFieldOutlet: UITextField!
+    @IBOutlet private weak var textFieldWeak: UITextField!
     @IBOutlet private var datePickerOutlet: UIDatePicker!
     @IBOutlet private var setThisTimeOutlet: UIButton!
     @IBOutlet private var labelTimeOutlet: UILabel!
     @IBOutlet private var switchOutlet: UISwitch!
     @IBOutlet private var clearOutlet: UIButton!
     
-    let datePicker = UIDatePicker()
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         setupUI()
         setValueForTextField()
-        addDoneButtonTo(textFieldOutlet)
+        datePickerOutlet.datePickerMode = .time
+        addDoneButtonTo(textFieldWeak)
     }
 
     @IBAction func sliderAction(_ sender: UISlider) {
-        textFieldOutlet.text = string(from: sender)
+        textFieldWeak.text = string(from: sender)
         progressViewOutlet.progress = sliderOutlet.value
     }
     
-    @IBAction func setThisTimeActionButton() {
-        getDateFromPicker()
+    @IBAction func setThisTimeActionButton(_ sender: UIButton) {
+        let formatter = DateFormatter()
+        formatter.timeStyle = .short
+        formatter.dateFormat = "HH:mm"
+        labelTimeOutlet.text = formatter.string(from: datePickerOutlet.date)
     }
     
     
@@ -46,20 +48,35 @@ class ViewController: UIViewController {
         } else {
             clearOutlet.isEnabled = false
             setThisTimeOutlet.isEnabled = false
-            labelTimeOutlet.text = "00 : 00"
+            labelTimeOutlet.text = "00:00"
         }
     }
     
     
     @IBAction func clearActionButton(_ sender: UIButton) {
-        
+        labelTimeOutlet.text = ""
+    }
+    
+    @IBAction func textFieldAction(_ sender: UITextField) {
+        guard let text = sender.text else { return }
+
+        if let currentValue = Float(text) {
+            switch sender.tag {
+            case 0:
+                sliderOutlet.value = currentValue
+                progressViewOutlet.progress = currentValue
+            default: break
+            }
+        } else {
+            showAlert(title: "Wrong format!", message: "Please enter correct value")
+        }
     }
     
     // MARK: - Private
     
     private func setupUI() {
-        textFieldOutlet.text = String(sliderOutlet.value)
-        textFieldOutlet.layer.cornerRadius = 10
+        textFieldWeak.text = String(sliderOutlet.value)
+        textFieldWeak.layer.cornerRadius = 10
         setThisTimeOutlet.layer.cornerRadius = 15
         labelTimeOutlet.layer.masksToBounds = true
         labelTimeOutlet.layer.cornerRadius = 15
@@ -67,46 +84,26 @@ class ViewController: UIViewController {
     }
     
     private func setValueForTextField() {
-        textFieldOutlet.text = string(from: sliderOutlet)
+        textFieldWeak.text = string(from: sliderOutlet)
     }
     
     private func string(from slider: UISlider) -> String {
         return String(format: "%.2f", slider.value)
-    }
-    
-    private func getDateFromPicker() {
-        datePicker.datePickerMode = .time
-        let formater = DateFormatter()
-        formater.dateFormat = "HH : mm"
-        labelTimeOutlet.text = formater.string(from: datePicker.date)
     }
 }
 
 // MARK: - ViewController + UITextFieldDelegate
 
 extension ViewController: UITextFieldDelegate {
-    // Скрываем клавиатуру нажатием на "Done"
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
         return true
     }
     
-    // Скрытие клавиатуры по тапу за пределами Text View
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super.touchesBegan(touches, with: event)
         
-        view.endEditing(true) // Скрывает клавиатуру, вызванную для любого объекта
-    }
-    
-    func textFieldDidEndEditing(_ textField: UITextField) {
-        guard let text = textField.text else { return }
-        
-        if let currentValue = Float(text) {
-            switch textField.tag {
-            case 0: sliderOutlet.value = currentValue
-            default: break
-            }
-        }
+        view.endEditing(true)
     }
 }
 
@@ -131,5 +128,12 @@ extension ViewController {
 
     @objc private func didTapDone() {
         view.endEditing(true)
+    }
+    
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
     }
 }
