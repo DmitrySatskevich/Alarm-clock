5cf5cd51b2fa3fa72226c87aca9b7e22dffc8fa4
RecrutSatsckevich <100848161+RecrutSatsckevich@users.noreply.github.com>
2022-12-07 17:01:30 +0300

    DS_Store

--- Alarm-clock/Alarm-clock/ViewController.swift
+++ Alarm-clock/Alarm-clock/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet private var progressViewOutlet: UIProgressView!
     @IBOutlet private var sliderOutlet: UISlider!
